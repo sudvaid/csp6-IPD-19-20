e91a693aab9a177f5569edfa750a4adf68c21d20
sudvaid <sudikshavaidy@gmail.com>
2020-02-19 21:40:35 +0000

    Changed Example 1 to a new strategy in which I will betray if their score is greater than mine.

--- example1.py
+++ example1.py
@@ -1,25 +1,23 @@
-####
-# Each team's file must define four tokens:
-#     team_name: a string
-#     strategy_name: a string
-#     strategy_description: a string
-#     move: A function that returns 'c' or 'b'
-####
+##### Each team's file must define four tokens:# team_name: a string# strategy_name: a string# strategy_description: a string# move: A function that returns 'c' or 'b'####
+
 
 team_name = 'E1'
-strategy_name = 'Betray'
-strategy_description = 'Always betray.'
-    
+
+strategy_name = 'Based on Score'
+
+strategy_description = 'If their score greater, betray.'
+
+def test(their_score, my_score): 
+  if their_score > my_score: 
+    return True 
+  else: 
+    return False
+
 def move(my_history, their_history, my_score, their_score):
-    '''Make my move based on the history with this player.
+   '''Make my move based on the history with this player. history: a string with one letter (c or b) per round that has been played with this opponent. their_history: a string of the same length as history, possibly empty. The first round between these two players is my_history[0] and their_history[0] The most recent round is my_history[-1] and their_history[-1] Returns 'c' or 'b' for collude or betray.''' 
+   
+   if len(my_history) == 0: 
+    return 'c'
     
-    history: a string with one letter (c or b) per round that has been played with this opponent.
-    their_history: a string of the same length as history, possibly empty. 
-    The first round between these two players is my_history[0] and their_history[0]
-    The most recent round is my_history[-1] and their_history[-1]
-    
-    Returns 'c' or 'b' for collude or betray.
-    '''
-    
-    #This example player always betrays.      
-    return 'b'
+  elif test(their_score, my_score) == True: 
+    return 'b' else: return 'c'
